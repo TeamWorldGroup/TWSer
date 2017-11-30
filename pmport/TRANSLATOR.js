00cac05ce8da11f13fb27948eb55820e201f9ce7
PROPHESSOR <xenteze@gmail.com>
2017-11-30 04:52:07 +0200

    Предпоследние правки, спать хочу

--- pmport/TRANSLATOR.js
+++ pmport/TRANSLATOR.js
@@ -14,15 +14,26 @@ class Translator {
 
 	static get PHPfuncs() {
 		return ` //PHP API for JS
-		const array_sum = x => {
-			let sum=0;
-			for(let i = 0; i < x.length; i++){
-				sum+=x[i];
-			}
-			return sum;
-		};
-		const count = x => x.length;
-		const round = (x, y) => x.toFixed(y);`;
+const array_sum = x => {
+	let sum=0;
+	for(let i = 0; i < x.length; i++){
+		sum+=x[i];
+	}
+	return sum;
+};
+const count = x => x.length;
+const round = (x, y) => x.toFixed(y);
+const strtolower = x => x.toLowerCase();
+		
+const stripos = ( f_haystack, f_needle, f_offset=0){
+	let haystack = f_haystack.toLowerCase();
+	let needle = f_needle.toLowerCase();
+	let index = 0;
+	if((index = haystack.indexOf(needle, f_offset)) > -1) {
+		return index;
+	}
+	return false;
+}`;
 	}
 
 	translate(fnc) {
@@ -32,7 +43,7 @@ class Translator {
 	}
 
 	save(name) {
-		fs.writeFileSync(name, Translator.PHPfuncs+this.file, 'utf8');
+		fs.writeFileSync(name, Translator.PHPfuncs + this.file, 'utf8');
 	}
 }
 
@@ -45,44 +56,49 @@ F.translate((x) => x.replace(/\./g, '+')) //Гребанная пхпшная к
 	.translate((x) => x.replace(/::/g, '.')) //Статические параметры
 	.translate((x) => x.replace(/\$/g, ''))
 	//Бездолларовые переменные
-	.translate((x) => x.replace(/use /g,'//import '))
+	.translate((x) => x.replace(/use /g, '//import '))
 	//Шпора для импортов
-	.translate((x) => x.replace(/namespace /g,'//extends '))
+	.translate((x) => x.replace(/namespace /g, '//extends '))
 	//У нас будет объектная структура, как в JsOS
-	.translate((x) => x.replace(/declare\(strict_types=1\);/g,'"use strict";'))
+	.translate((x) => x.replace(/declare\(strict_types=1\);/g, '"use strict";'))
 	//Строгий режим
-	.translate((x) => x.replace(/ : (int|bool|string|float|array)/g, ''))
+	.translate((x) => x.replace(/ : (int|bool|string|float|array|void|\?\w*)/g, ''))
 	//У нас динамическая типизация
-	.translate((x) => x.replace(/(<\?php|\?>|<\?)/g,''))
+	.translate((x) => x.replace(/(<\?php|\?>|<\?)/g, ''))
 	//Это уже не PHP
 	//Поехали херить классы...
 	//TODO: Константы в начале нужно ручками запихнуть в конструктор и под this
-	.translate((x) => x.replace(/public /g,''))
+	.translate((x) => x.replace(/public /g, ''))
 	//Нинада
-	.translate((x) => x.replace(/private /g,'_'))
+	.translate((x) => x.replace(/private /g, '_'))
 	//underscore
-	.translate((x) => x.replace(/const /g,''))
+	.translate((x) => x.replace(/const /g, ''))
 	//Тоже нинада
-	.translate((x) => x.replace(/function /g,''))
+	.translate((x) => x.replace(/function /g, ''))
 	//Там везде ООП, так что тоже не надобно
-	.translate((x) => x.replace(/(int|float|bool|string|array) /g,''))
+	.translate((x) => x.replace(/(int|float|bool|string|array) /g, ''))
 	//TODO: Увы, кастомные типы я не буду сюда пихать
 	//Убираем типизацию
-	.translate((x) => x.replace(/min\(/g,'Math.min(')
-		.replace(/max\(/g,'Math.max('))
+	.translate((x) => x.replace(/min\(/g, 'Math.min(')
+		.replace(/max\(/g, 'Math.max('))
 	//Mathermatika
 	.translate((x) => x.replace(/class /g, '//TODO: Оберни константы в конструктор!\nclass '))
 	//Лучше ещё раз напомнить
+	.translate((x) => x.replace(/foreach\((.+) as (.+) => (.+)\){/g, 'for(let $2 in $1){\nconst $3 = $1[$2]'))
 	.translate((x) => x.replace(/foreach\((.+) as (.+)\)/g, 'for(const $2 of $1)'))
 	//Переработка циклов
-	.translate((x) => x.replace(/throw new .+\(/g,'throw new Error('))
+	.translate((x) => x.replace(/throw new .+\(/g, 'throw new Error('))
 	//Нам и одного типа ошибок хватит
 	.translate((x) => x.replace(/\((int|float)\) (.+)\s+([:|;?<>=!,.^%+-/*&~])/g, 'Number($2) $3'))
 	//По-любому какой-то символ не доглядел
 	//Заменяет (int) her на Number(her)
 	.translate((x) => x.replace(/\((string)\) (.+)\s+([:|;?<>=!,.^%+-/*&~])/g, 'String($2) $3'))
 	//(string) her -> String(her)
-	.translate((x) => x.replace(/\(bool\) /g, '!!'));
+	.translate((x) => x.replace(/\(bool\) /g, '!!'))
 	//(bool) her -> !!her
+	.translate((x) => x.replace(/ or /g, ' || ')
+		.replace(/ and /g, ' && ')
+		.replace(/ \?\? /g, ' || ')); // her || null
+//Логические операторы прямиком из Basic'а
 
 F.save('./Translate/server.js');
